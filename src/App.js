3f952181ba8f165ae1b664260275cf838abddbe8
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-02-27 23:30:31 +0100

    Create SPA

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path='/' component={Navigation}>
+                    <IndexRoute component={Home} />
+                    <Route path='/contact' component={Contact} />
+                    <Route path='/hello/:name' component={Hello} />
+                    <Route path='*' component={PageNotFound} />
+                </Route>
+            </Router>
+        );
+    }
 }
+
+const Navigation = props => (
+    <div>
+        <ul>
+            <li><Link to='/' activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Home</Link></li>
+            <li><Link to='/contact' activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Contact</Link></li>
+        </ul>
+    {props.children}
+    </div>
+);
+
+const Home = () => <h1>Home component</h1>
+const Contact = () => <h1>Contact component</h1>
+const PageNotFound = () => <h1>404 Not found</h1>
+const Hello = (props) => <h1>Hello, {props.params.name}</h1>
 
 export default App;
